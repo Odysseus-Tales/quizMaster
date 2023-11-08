81731f9ad96e2afb77dcc37ce1d6aa2f96ccc527
Odysseus-Tales <142748027+Odysseus-Tales@users.noreply.github.com>
2023-11-08 10:46:46 -0500

    Swapping sprites

--- quizMaster/Assets/Scripts/Quiz.cs
+++ quizMaster/Assets/Scripts/Quiz.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
 
     void Start()
     {
@@ -21,6 +26,26 @@ public class Quiz : MonoBehaviour
         
     }
 
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "correct";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex)
+            questionText.text = "sorry, the correct answer was;\n"+correctAnswer;
+            buttonImage = answersButtons[correctAnswerIndex].GetComponent<buttonImage>();
+            buttonImage.sprite = correctAnswerSprite;
+            
+        }
+    }
+-
 
 
 }
